4b88d153470f0843b650e54c6638c75da4097729
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2025-01-27 12:29:00 +0000

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -29,6 +29,7 @@ router.post('/api/paytech/ipn', (req, res) => {
     console.log("venant api_secret", api_secret_sha256)
     console.log("local api_secret", my_api_secret)
     console.log("LE BODY", req.body)
+    return
 
     // if (
     //     SHA256Encrypt(my_api_secret) === api_secret_sha256 &&
